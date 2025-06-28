f42441f22f1f7e33872fc903f0e15e5e9b25ed62
Krex381 <124784791+Krex381@users.noreply.github.com>
2025-06-28 18:54:56 +0200

    Add files via upload

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/next"
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -71,6 +72,7 @@ export default function RootLayout({
         <div id="root" className="min-h-screen">
           {children}
         </div>
+        <Analytics />
       </body>
     </html>
   );
